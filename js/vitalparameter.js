012f8749381242ad4ff9e4ee9526115470ba2979
johanna illner <jojo@johannas-MacBook-Air.local>
2018-10-18 13:18:30 +0200

    Blutdruck Liste mit jquery befüllen fertiggestellt
    Co-Authored-By: null <JohannaIllner@users.noreply.github.com>

--- js/vitalparameter.js
+++ js/vitalparameter.js
@@ -1,12 +1,16 @@
-//Wert aus erstem Hinzufügen Feld auslesen
+//Werte von Blutdruck Feldern auslesen
 $('#hinzu1').click(function() {
-    var wert1 = $("#systRR").val();   //.val --> Wert des Feldes in Variable speichern
-    var wert2 =$("#diastRR").val();
+    var wert1 = $("#systRR").val(); //.val --> Wert des Feldes in Variable speichern
+    var wert2 = $("#diastRR").val();
+    var zeit = $("#uhrRR").val();
 
-//Ausgelesene Werte speichern
-// Variable feld1 erstellen, die die ausgelesenen Werte erhält
-var feld1 ="<li class=\"list-group-item\">" + wert1 + " / " + wert2 + " mmHg - 16:35 Uhr</li>";
-$( "#RRListe" ).append(feld1);
-//Werte in Liste einfügen
-    $("#hinzu1").val("Gesetzter Wert");
+    //Ausgelesene Werte speichern
+    // Variable feld1 erstellen, die die ausgelesenen Werte erhält
+    var feld1 = "<li class=\"list-group-item\">" + wert1 + " / " + wert2 + " mmHg - " + zeit + " Uhr</li>";
+    //Werte (feld1) in Liste einfügen
+    $("#RRListe").append(feld1);
+    //ausgelesene Felder wieder leeren
+    $('#systRR').val(''); //Value auf leer setzen
+    $('#diastRR').val('');
+    $('#uhrRR').val('');
 });
