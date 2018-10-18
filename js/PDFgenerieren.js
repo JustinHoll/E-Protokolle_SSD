c38f2aaf1ccc4e7a4c0e8a0a0d64cbb866dfe830
johanna illner <jojo@johannas-MacBook-Air.local>
2018-10-18 12:13:10 +0200

    PDFConverter hinzufügen
    Co-Authored-By: null <JohannaIllner@users.noreply.github.com>

---
+++ js/PDFgenerieren.js
@@ -0,0 +1,3 @@
+//Code zum konvertieren zu PDF
+var element = document.getElementById('element-to-print');
+html2pdf(element);
--- js/reiterUmschalten.js
+++ js/reiterUmschalten.js
@@ -1,6 +1,10 @@
 $('.reiterBtn').click(function(event){
     $('.reiter').addClass("hidden");
+    // ID von geklickten Button
     $('#'+event.target.id+'Div').removeClass("hidden");
+
+    //Klassen von bisher aktivem Button removen
     $('.aktiverBtn').removeClass("aktiverBtn");
+    //dem neu ausgewählten Button die Klasse hinzufügen
     $('#'+event.target.id).addClass("aktiverBtn");
 });
