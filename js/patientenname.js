e8315d92826d6d3eccf558fdda84f9896bee6ea1
jonasb007 <josojaramail@gmail.com>
2018-10-19 14:23:30 +0200

    Leerer Vorname + Nachname (bisher nur einmalig)

--- js/patientenname.js
+++ js/patientenname.js
@@ -4,10 +4,16 @@ $(".nameeingabe").blur(function() {
 
 
 //bei Klick auf den Patientennamen kommt man zum Feld um den Vornamen einzutragen
-$("#name").click(function(){
+$("#name").click(function() {
     $('.reiter').addClass("hidden");
     $('#patientendatenDiv').removeClass("hidden");
     $('.aktiverBtn').removeClass("aktiverBtn");
     $('#patientendaten').addClass("aktiverBtn");
     $('#vorname').focus();
 });
+
+//wenn die vorname und nachname-felder leer sind, soll Patientenname als Füller erscheinen
+if ($(".nameeingabe").val() == "") {
+    alert('Leerer Vorname + Nachname');
+    $("#name").text("Patientenname");
+}
