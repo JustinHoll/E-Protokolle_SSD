18c2c532d7b993a95e79c7c63f6eb8ba5083cc8f
Justin Hollmann <justin.hollmann.jh@gmail.com>
2018-10-24 16:58:06 +0200

    Sanitäter 1 in Variable speichern

--- js/protokollgenerierung.js
+++ js/protokollgenerierung.js
@@ -1,3 +1,7 @@
 $('#ausblenden').click(function() {
-    
-}
+    //alle Daten in Variablen speichern
+    var san1 = $("#san1").val();
+    alert("San1: " + san1); // diese Zeile ist nur zum Testen, ob Sanitäter 1 erfolgreich rausgezogen wurde
+
+
+});
