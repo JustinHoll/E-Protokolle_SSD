a98c2645f72000eb795a9722abd7da81e8bc7870
johanna illner <jojo@johannas-MacBook-Air.local>
2018-10-27 20:16:50 +0200

    Kommentare hinzugefügt

--- js/reiterUmschalten.js
+++ js/reiterUmschalten.js
@@ -2,9 +2,11 @@ var aktuelleReiterId = "einsatzinformationen";
 
 $('.reiterBtn').click(function(event){
     $('.reiter').addClass("hidden");
+
     // ID von geklickten Button
     $('#'+event.target.id+'Div').removeClass("hidden");
-    //Klassen von bisher aktivem Button removen
+
+    //Klasse "aktiver Button" von bisher aktivem Button removen
     $('.aktiverBtn').removeClass("aktiverBtn");
     //dem neu ausgewählten Button die Klasse hinzufügen
     $('#'+event.target.id).addClass("aktiverBtn");
