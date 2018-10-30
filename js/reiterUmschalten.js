4075c0e0cd960064f09e2fed2eed79356c418336
jonasb007 <josojaramail@gmail.com>
2018-10-30 16:07:20 +0100

    app.js entfernt

--- js/reiterUmschalten.js
+++ js/reiterUmschalten.js
@@ -1,15 +1,15 @@
 var aktuelleReiterId = "einsatzinformationen";
 
-$('.reiterBtn').click(function(event){
+$('.reiterBtn').click(function(event) {
     $('.reiter').addClass("hidden");
 
     // ID von geklickten Button
-    $('#'+event.target.id+'Div').removeClass("hidden");
+    $('#' + event.target.id + 'Div').removeClass("hidden");
 
     //Klasse "aktiver Button" von bisher aktivem Button removen
     $('.aktiverBtn').removeClass("aktiverBtn");
     //dem neu ausgewählten Button die Klasse hinzufügen
-    $('#'+event.target.id).addClass("aktiverBtn");
+    $('#' + event.target.id).addClass("aktiverBtn");
 
     aktuelleReiterId = event.target.id;
 });
@@ -17,36 +17,36 @@ $('.reiterBtn').click(function(event){
 //Tab Umschalten mit Pfeiltasten
 $(document).on('keydown', function(e) {
     var tag = e.target.tagName.toLowerCase();
-    if ( (e.which === 39 || e.which === 40) && tag != 'input' && tag != 'textarea'){
-        var naechsterReiter = $("#"+aktuelleReiterId).next().attr("id");
+    if ((e.which === 39 || e.which === 40) && tag != 'input' && tag != 'textarea') {
+        var naechsterReiter = $("#" + aktuelleReiterId).next().attr("id");
 
-        if(naechsterReiter == "btnAbschließen"){
+        if (naechsterReiter == "btnAbschließen") {
             naechsterReiter = "einsatzinformationen";
         }
 
         $('.reiter').addClass("hidden");
         // ID von geklickten Button
-        $('#'+naechsterReiter+'Div').removeClass("hidden");
+        $('#' + naechsterReiter + 'Div').removeClass("hidden");
         //Klassen von bisher aktivem Button removen
         $('.aktiverBtn').removeClass("aktiverBtn");
         //dem neu ausgewählten Button die Klasse hinzufügen
-        $('#'+naechsterReiter).addClass("aktiverBtn");
+        $('#' + naechsterReiter).addClass("aktiverBtn");
 
         aktuelleReiterId = naechsterReiter;
-    }else if((e.which === 37 || e.which === 38) && tag != 'input' && tag != 'textarea'){
-        var naechsterReiter = $("#"+aktuelleReiterId).prev().attr("id");
+    } else if ((e.which === 37 || e.which === 38) && tag != 'input' && tag != 'textarea') {
+        var naechsterReiter = $("#" + aktuelleReiterId).prev().attr("id");
 
-        if(naechsterReiter === void(0)){
+        if (naechsterReiter === void(0)) {
             naechsterReiter = "notizen";
         }
 
         $('.reiter').addClass("hidden");
         // ID von geklickten Button
-        $('#'+naechsterReiter+'Div').removeClass("hidden");
+        $('#' + naechsterReiter + 'Div').removeClass("hidden");
         //Klassen von bisher aktivem Button removen
         $('.aktiverBtn').removeClass("aktiverBtn");
         //dem neu ausgewählten Button die Klasse hinzufügen
-        $('#'+naechsterReiter).addClass("aktiverBtn");
+        $('#' + naechsterReiter).addClass("aktiverBtn");
 
         aktuelleReiterId = naechsterReiter;
     }
